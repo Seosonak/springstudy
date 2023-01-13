b9821bbfa6a8215055d26de4f4e93803b4b117e6
Seosonak <nakoa346@gmail.com>
2023-01-13 18:09:06 +0900

    0113

--- jpa-test/src/main/java/com/example/constant/MemberClass.java
+++
@@ -1,7 +0,0 @@
-package com.example.constant;
-
-//열거형상수 (enum)
-public enum MemberClass {
-
-	USER, VIP, ADMIN
-}
--- jpa-test/src/main/java/com/example/entity/emf/UniqueEntityManagerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.example.entity.emf;
-
-import javax.persistence.EntityManagerFactory;
-
-public class UniqueEntityManagerFactory {
-//엔티티매니저팩토리. 하나! 그래서 여기저기에서 쓰기위해 static! 
-	public static EntityManagerFactory emf;
-}
